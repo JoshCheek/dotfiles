b579b5e9a920a4081597f026f1fe604e81af26d8
Josh Cheek <josh.cheek@gmail.com>
2014-06-21 10:57:58 -0600

    Extract fish print-ary function into generic show-argv binary

--- fish/functions/print-ary.fish
+++
@@ -1,7 +0,0 @@
-function print-ary --description 'Prints a fish array with each entry on its own line'
-  # should it error if argv is emptpy?
-  for entry in $argv
-    echo $entry
-  end
-  return 0
-end
