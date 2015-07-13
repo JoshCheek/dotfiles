42b8e825bc91db1baea906f1aa49817400d8d47b
Josh Cheek <josh.cheek@gmail.com>
2015-07-13 12:15:30 -0600

    Revert "Animated prompt! We'll see if I like it or if its gets annoying :P"
    This reverts commit 61aa83caeab3add887121ce7e3687d9df53cc3e1.
    
    It wasn't too annoying, but it got a little buggy, overwriting the last
    line of output.

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,6 +1,6 @@
-# Screenshot: https://s3.amazonaws.com/josh.cheek/images/scratch/fish-prompt.gif
+function fish_prompt
+  set -l last_status $status
 
-function fish_prompt-status_bar -d 'Info I want to see just above my prompt'
   # the time
   set_color --background magenta
   echo -n (date +" %I:%M %p ")
@@ -20,54 +20,14 @@ function fish_prompt-status_bar -d 'Info I want to see just above my prompt'
     set_color --background normal
   end
 
-  # next line
-  echo
-end
-
-
-function fish_prompt-colour -d 'blue if the exit status is successful, red if unsuccessful'
-  if [ $argv[1] = 0 ]
-    echo blue
+  # the prompt depends on the last status
+  if [ $last_status = 0 ]
+    echo -n \n"🐠 "
   else
-    echo red
+    echo -n \n"🍣 " # sushi
+    # other option: 🎣
   end
-end
+  set_color --background normal
+  echo -n ' '
 
-
-function fish_prompt-animated_prompt -e fish_postexec -d 'Animates a swimming fishy ^_^'
-  set -l last_status $status
-
-  fish_prompt-status_bar
-  set_color (fish_prompt-colour $last_status)
-
-  for swimmy_fishy in   "><(((º> "\
-                      \r".><(((º> "\
-                      \r"¸.><(((º> "\
-                      \r".¸.><(((º> "\
-                      \r"·.¸.><(((º> "\
-                      \r"`·.¸.><(((º> "\
-                      \r"¯`·.¸.><(((º> "\
-                      \r"´¯`·.¸.><(((º> "\
-                      \r"·´¯`·.¸.><(((º> "\
-                      \r".·´¯`·.¸.><(((º> "\
-                      \r"¸.·´¯`·.¸.><(((º> "\
-                      \r"¸¸.·´¯`·.¸.><(((º> "\
-                      \r".¸¸.·´¯`·.¸.><(((º> "\
-                      \r"·.¸¸.·´¯`·.¸.><(((º> "
-    sleep 0.01
-    echo -n $swimmy_fishy
-  end
-end
-
-
-function fish_prompt -d 'Redraw prompt after animating so that Fish can reason about it'
-  set -l last_status $status
-
-  # go up 2 lines
-  echo -n \e"[2A"
-
-  fish_prompt-status_bar
-  set_color (fish_prompt-colour $last_status)
-  echo -n "·.¸¸.·´¯`·.¸.><(((º> "
-  set_color normal
 end
